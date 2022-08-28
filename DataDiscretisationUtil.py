594f0c1228f6072eb98329180e77cff773fe7d09
Raghunandan Arava <84313718+raghunandanarava@users.noreply.github.com>
2022-08-28 10:15:09 +0200

    Delete DataDiscretisationUtil.py

--- DataDiscretisationUtil.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import numba as nb
-
-@nb.jit(nopython=True)
-def vrange(starts, stops, steps):
-    discretised = []
-    for i in range(len(starts)):
-        discretised.append(np.arange(start=starts[i], stop=stops[i], step=steps))
-    return np.asarray(np.column_stack([discretised, stops]))
